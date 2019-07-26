eb45770308553cb501156250faca4bac425da1f1
Tommy <tommy@localhost.localdomain>
2019-07-26 15:24:55 +0200

    Changed to array of Frames, added several loops

--- Game.java
+++ Game.java
@@ -1,78 +1,50 @@
 public class Game
 {
-	public Frame frame1;
-	public Frame frame2;
-	public Frame frame3;
-	public Frame frame4;
-	public Frame frame5;
-	public Frame frame6;
-	public Frame frame7;
-	public Frame frame8;
-	public Frame frame9;
-	public Frame frame10;
+	public Frame[] bowling = new Frame[10];
 
 	public Game()
 	{
-		frame1 = new Frame(false);
-		frame2 = new Frame(false);
-		frame1.setNext(frame2);
-		frame3 = new Frame(false);
-		frame2.setNext(frame3);
-		frame4 = new Frame(false);
-		frame3.setNext(frame4);
-		frame5 = new Frame(false);
-		frame4.setNext(frame5);
-		frame6 = new Frame(false);
-		frame5.setNext(frame6);
-		frame7 = new Frame(false);
-		frame6.setNext(frame7);
-		frame8 = new Frame(false);
-		frame7.setNext(frame8);
-		frame9 = new Frame(false);
-		frame8.setNext(frame9);
-		frame10 = new Frame(true);
-		frame9.setNext(frame10);
+		for(int i=0;i<bowling.length-1;i++)
+			bowling[i] = new Frame(false);
+		bowling[9] = new Frame(true);
+		for(int i=1;i<bowling.length;i++)
+			bowling[i-1].setNext(bowling[i]);
 	}
 
 	private int frameScore(Frame f)
 	{
-		int points=0;
 		if(f.getThrow1()==10)//is this a strike?
 			if(f.isLast())//is it last frame?
-				points+=10+f.getBonus1()+f.getBonus2();
+				return 10+f.getBonus1()+f.getBonus2();
 			else if(f.getNext().getThrow1()==10)//is next frame a strike?
 				if(f.getNext().isLast())//is it second to last frame?
-					points+=20+f.getNext().getBonus1();
+					return 20+f.getNext().getBonus1();
 				else
-					points+=20+f.getNext().getNext().getThrow1();
+					 return 20+f.getNext().getNext().getThrow1();
 			else
-				points+=10+f.getNext().getThrow1()+f.getNext().getThrow2();
+				return 10+f.getNext().getThrow1()+f.getNext().getThrow2();
 		else if((f.getThrow1()+f.getThrow2())==10)//is this a spare?
 			if(f.isLast())//is it last frame?
-				points+=10+f.getBonus1();
+				return 10+f.getBonus1();
 			else
-				points+=10+f.getNext().getThrow1();
+				return 10+f.getNext().getThrow1();
 		else
-			points+=f.getThrow1()+f.getThrow2();
-		return points;
+			 return f.getThrow1()+f.getThrow2();
 	}
 
 	public int gameScore()
 	{
 		int points=0;
-		Frame f = frame1;
-		while(!f.isLast())
-		{
+		for(Frame f:bowling)
 			points+=frameScore(f);
-			f=f.getNext();
-		}
-		points+=frameScore(f);
 		return points;
 	}
 
 	public String toString()
 	{
-		return "Frames: " + frame1 + " | " + frame2 + " | " + frame3 + " | " + frame4 + " | " + frame5 + " | " +
-			frame6 + " | " + frame7 + " | " + frame8 + " | " + frame9 + " | " + frame10;
+		StringBuilder builder = new StringBuilder();
+		for(Frame f:bowling)
+			builder.append(f + " | ");
+		return builder.toString();
 	}
 }
