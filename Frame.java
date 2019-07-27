a9c065f4391a4882bbf2f74a4516b2a062100b57
Tommy <tommy@localhost.localdomain>
2019-07-27 13:57:42 +0200

    Javadoc comments added

--- Frame.java
+++ Frame.java
@@ -7,9 +7,15 @@ public class Frame
 	private int bonus2;
 	private Frame next;
 
-	public Frame(boolean x)
+	/**
+	 * Creates a bowling frame containing of 2 throws.
+	 * There are several special cases, they are:
+	 * Strike, strike in last frame and spare in last frame.
+	 * @param last Boolean value if set to true if its last frame
+	 */
+	public Frame(boolean last)
 	{
-		lastFrame = x;
+		lastFrame = last;
 		if(lastFrame)
 		{
 			throw1=randomizer(10);
@@ -40,65 +46,103 @@ public class Frame
 		}
 	}//end constructor
 
+	/**
+	 * Constructor without boolean param, set boolean to false as default.
+	 */
+	public Frame()
+	{
+		this(false);
+	}//end constructor
+
+	/**
+	 * Randomizes the throw.
+	 * @param n Maximum value for the randomizer to reach
+	 * @return Value between 0 and n
+	 */
 	private int randomizer(int n)
 	{
 		return (int)(Math.random()*(n+1));
 	}//end randomizer
 
+	/**
+	 * Set up a link to the next frame, for use in score calculation
+	 * @param f Frame linked to
+	 */
 	public void setNext(Frame f)
 	{
 		next=f;
 	}//end setNext
 
+	/**
+	 * @return First throw
+	 */
 	public int getThrow1()
 	{
 		return throw1;
 	}//end getThrow1
 
+	/**
+	 * @return Second throw
+	 */
 	public int getThrow2()
 	{
 		return throw2;
 	}//end getThrow2
 
+	/**
+	 * @return First bonusthrow
+	 */
 	public int getBonus1()
         {
                 return bonus1;
         }//end getBonus1
 
+	/**
+	 * @return Second bonusthrow
+	 */
         public int getBonus2()
         {
                 return bonus2;
         }//end getBonus2
 
+	/**
+	 * @return Next frame
+	 */
 	public Frame getNext()
 	{
 		return next;
 	}//end getNext
 
+	/**
+	 * @return True if last frame
+	 */
 	public boolean isLast()
 	{
 		return lastFrame;
 	}//end isLast
 
+	/**
+	 * @return Throws counted this frame
+	 */
 	public String toString()
 	{
-		if(lastFrame)
-			if(throw1==10)
-				if(bonus1==10)
+		if(lastFrame)//last frame?
+			if(throw1==10)//strike?
+				if(bonus1==10)//strike again?
 					if(bonus2==10)
 						return "X X X";
 					else
 						return "X X " + bonus2;
 				else
 					return "X " + bonus1 + " " + bonus2;
-			else if((throw1+throw2)==10)
+			else if((throw1+throw2)==10)//spare?
 				return throw1 + " / " + bonus1;
 			else
 				return throw1 + " " + throw2;
 		else
-			if(throw1==10)
+			if(throw1==10)//strike?
 				return "X";
-			else if((throw1+throw2)==10)
+			else if((throw1+throw2)==10)//spare?
 				return throw1 + " /";
 			else
 				return throw1 + " " + throw2;
--- Game.java
+++ Game.java
@@ -2,15 +2,24 @@ public class Game
 {
 	private Frame[] bowling = new Frame[10];
 
+	/**
+	 * Creates a game of ten frames, where the last is flagged as last frame.
+	 * Also sets to order, used for scoring
+	 */
 	public Game()
 	{
 		for(int i=0;i<bowling.length-1;i++)
-			bowling[i] = new Frame(false);
+			bowling[i] = new Frame();
 		bowling[9] = new Frame(true);
 		for(int i=1;i<bowling.length;i++)
 			bowling[i-1].setNext(bowling[i]);
 	}//end constructor
 
+	/**
+	 * Calculates score for a single frame, check for last frame
+	 * @param f Frame to calculate score for
+	 * @return Score for a single frame
+	 */
 	private int frameScore(Frame f)
 	{
 		if(f.getThrow1()==10)//is this a strike?
@@ -32,6 +41,10 @@ public class Game
 			 return f.getThrow1()+f.getThrow2();
 	}//end frameScore
 
+	/**
+	 * Calculate score for whole game, added score for every frame together
+	 * @return Total score
+	 */
 	public int gameScore()
 	{
 		int points=0;
@@ -40,6 +53,9 @@ public class Game
 		return points;
 	}//end gameScore
 
+	/**
+	 * @return A formated string containing every throw in the game.
+	 */
 	public String toString()
 	{
 		StringBuilder builder = new StringBuilder();
